4e6ed7178a834445da471e10ab3c1adfd17ac7be
acolombe <alexis.colombe@live.fr>
2016-09-20 11:00:26 +0200

    Update order.class.php

--- inc/order.class.php
+++ inc/order.class.php
@@ -1714,7 +1714,10 @@ class PluginOrderOrder extends CommonDBTM {
                $PluginOrderOrder_Item->getPricesATI($data["port_price"],
                                                     Dropdown::getDropdownName("glpi_plugin_order_ordertaxes",
                                                                               $data["plugin_order_ordertaxes_id"]));
-            $total +=  $prices["priceTTC"] + $postagewithTVA;
+            // If order isn't canceled nor deleted
+            if ($data['plugin_order_orderstates_id'] < 5) {
+               $total +=  $prices["priceHT"];
+            }
             $link   = Toolbox::getItemTypeFormURL(__CLASS__);
 
             echo "<tr class='tab_bg_1' align='center'>";
